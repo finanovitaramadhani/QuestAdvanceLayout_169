c6fcefd6da26396da4e0f709409c1cdf7fcb219b
finanovita <finanovita479@gmail.com>
2025-10-24 19:41:30 +0700

    Menambahkan Spacer untuk memberi jarak

--- app/src/main/java/com/example/myapplication/UIPertama.kt
+++ app/src/main/java/com/example/myapplication/UIPertama.kt
@@ -32,3 +32,4 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
             text = stringResource(id = R.string.univ),
             fontSize = 22.sp
         )
+        Spacer(modifier = Modifier.height(25.dp))
