45ae3b4bf54c547b1cccf1278165e85825b3c85a
finanovita <finanovita479@gmail.com>
2025-10-24 19:43:24 +0700

    Menambahkan Card dengan styling

--- app/src/main/java/com/example/myapplication/UIPertama.kt
+++ app/src/main/java/com/example/myapplication/UIPertama.kt
@@ -33,3 +33,12 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
             fontSize = 22.sp
         )
         Spacer(modifier = Modifier.height(25.dp))
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            ),
+            shape = RoundedCornerShape(12.dp)
+        )
