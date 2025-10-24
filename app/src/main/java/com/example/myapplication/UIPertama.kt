559e34af6668ef27730472f607776cc6db73cce7
finanovita <finanovita479@gmail.com>
2025-10-24 19:40:23 +0700

    Menambahkan Text untuk univ

--- app/src/main/java/com/example/myapplication/UIPertama.kt
+++ app/src/main/java/com/example/myapplication/UIPertama.kt
@@ -28,3 +28,7 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
             fontSize = 35.sp,
             fontWeight = FontWeight.Bold
         )
+        Text(
+            text = stringResource(id = R.string.univ),
+            fontSize = 22.sp
+        )
