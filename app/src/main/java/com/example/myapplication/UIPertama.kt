4ee221844e88ca648132e2746598ff992f256ec2
finanovita <finanovita479@gmail.com>
2025-10-24 19:49:17 +0700

    Menambahkan Column untuk nama dan alamat

--- app/src/main/java/com/example/myapplication/UIPertama.kt
+++ app/src/main/java/com/example/myapplication/UIPertama.kt
@@ -56,3 +56,20 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
                     contentScale = ContentScale.Crop
                 )
                 Spacer(modifier = Modifier.width(30.dp))
+                Column {
+                    Text(
+                        text = stringResource(id = R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.alamat),
+                        fontSize = 20.sp,
+                        color = Color.Yellow,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                }
+            }
+        }
