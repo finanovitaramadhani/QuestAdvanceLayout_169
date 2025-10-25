034728bea3a6cc2696359dbddae0bfc7e0425025
finanovita <finanovita479@gmail.com>
2025-10-25 20:50:49 +0700

    Membuat fungsi composable utama bernama AktivitasKedua

--- app/src/main/java/com/example/myapplication/UIKedua.kt
+++ app/src/main/java/com/example/myapplication/UIKedua.kt
@@ -31,3 +31,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+
+@Composable
+fun AktivitasKedua(modifier: Modifier = Modifier) {
